1fe1c9be1a64bdf4f91d1c191d55ea668d8c74d2
Markus Veloz <m.veloz@calltrade.ch>
2021-05-26 15:11:12 +0200

    upgraded angular to version 11.2.14

--- src/app/components/invest/invest.component.spec.ts
+++ src/app/components/invest/invest.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { InvestComponent } from './invest.component';
 
@@ -6,7 +6,7 @@ describe('InvestComponent', () => {
   let component: InvestComponent;
   let fixture: ComponentFixture<InvestComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ InvestComponent ]
     })
